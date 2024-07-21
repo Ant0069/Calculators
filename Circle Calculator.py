650e20bdf2f3d81d63d2aee47dab15cde7b27f31
Ant0069 <167881182+Ant0069@users.noreply.github.com>
2024-07-21 18:20:06 +0800

    Update circle

--- Circle Calculator.py
+++ Circle Calculator.py
@@ -11,3 +11,17 @@ print(f"Diameter is {Diameter}")
 print(f"Radius is {Radius}")
 print(f"Circumference is {Circumference}")
 print(f"Area is {Area}")
+
+Semi_Circle = input("Do you need semi circle calculations ")
+if Semi_Circle == "Yes" or "yes":
+    Diameter_Semi = float(input("Enter Diameter"))
+    Radius_Semi = Diameter_Semi /2 or float(input("Enter Radius"))
+    Diameter_Semi = Radius_Semi * 2
+    Circumference_Semi = (2 * pi * Radius)/2
+    Area_Semi = pi*(Radius**2)/2
+
+    print("Calculation complete")
+    print(f"Diameter is {Diameter_Semi}")
+    print(f"Radius is {Radius_Semi}")
+    print(f"Circumference is {Circumference_Semi}")
+    print(f"Area is {Area_Semi}")
