3e1e8b9a25ff3c61e648948e1f2eec712ca504ff
Ant0069 <167881182+Ant0069@users.noreply.github.com>
2024-07-26 16:36:33 +0800

    UpdateV0.1

--- Circle Calculator.py
+++ Circle Calculator.py
@@ -1,10 +1,11 @@
 import math
 import tkinter as tk
 from tkinter.simpledialog import askstring
+pi = math.pi
 
 Windows = tk.Tk()
 Windows.title("Circle Calculator")
-pi = math.pi
+Windows.geometry("200x200")
 Diameter = float(askstring("Diameter", "Enter diameter "))
 Radius = Diameter/2 or float(askstring("Radius", "Enter Radius "))
 Diameter = Radius * 2
@@ -27,20 +28,22 @@ Windows_Semi = tk.Tk()
 Windows_Semi.title("Semi Circle Calculator")
 Semi_Circle = str(askstring("Semi Circles", "Do you need semi circle calculations ")).lower
 if Semi_Circle == "yes":
-    Diameter_Semi = float(askstring("Diamter", "Enter Diameter"))
-    Radius_Semi = Diameter_Semi/2 or float(askstring("Radius", "Enter Radius"))
-    Diameter_Semi = Radius_Semi * 2
-    Circumference_Semi = (2 * pi * Radius)/2
-    Area_Semi = pi*(Radius**2)/2
+   Diameter_Semi = float(askstring("Diamter", "Enter Diameter"))
+   Radius_Semi = Diameter_Semi/2 or float(askstring("Radius", "Enter Radius"))
+   Diameter_Semi = Radius_Semi * 2
+   Circumference_Semi = (2 * pi * Radius_Semi)/2
+   Area_Semi = pi*(Radius_Semi**2)/2
 
-    Semi_Diameter_Ans = tk.Label(text=f"Diameter is {Diameter_Semi}")
-    Semi_Diameter_Ans.pack()
-    Semi_Radius_Ans = tk.Label(text=f"Radius is {Radius_Semi}")
-    Semi_Radius_Ans.pack()
-    Semi_Circumfernce_Ans = tk.Label(text=f"Circumfernce is {Circumference_Semi}")
-    Semi_Circumfernce_Ans.pack()
-    Semi_Area_Ans = tk.Label(text=f"Area is {Area_Semi}")
-    Semi_Area_Ans.pack()
+   semi_Diameter_Ans = tk.Label(text=f"Diameter is {Diameter_Semi}")
+   Semi_Radius_Ans = tk.Label(text=f"Radius is {Radius_Semi}")
+   Semi_Circumfernce_Ans = tk.Label(text=f"Circumfernce is {Circumference_Semi}")
+   Semi_Area_Ans = tk.Label(text=f"Area is {Area_Semi}")
 
+   semi_Diameter_Ans.pack()
+   Semi_Radius_Ans.pack()
+   Semi_Circumfernce_Ans.pack()
+   Semi_Area_Ans.pack()
 else:
-    exit()
+   exit()
+
+
