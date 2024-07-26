1167709fdf9242acff8f1ccd6c33a9a3ec17901c
Ant0069 <167881182+Ant0069@users.noreply.github.com>
2024-07-26 08:39:49 +0800

    Update

--- Circle Calculator.py
+++ Circle Calculator.py
@@ -8,8 +8,7 @@ pi = math.pi
 Diameter = float(askstring("Diameter", "Enter diameter "))
 Radius = Diameter/2 or float(askstring("Radius", "Enter Radius "))
 Diameter = Radius * 2
-Circumference = 2*pi * \
-    Radius or float(askstring("Circumference", "Enter Circumfernce "))
+Circumference = 2*pi *Radius or float(askstring("Circumference", "Enter Circumfernce "))
 Area = pi*(Radius**2) or float(askstring("Area", "What is the Area"))
 
 
@@ -24,20 +23,24 @@ Circumference_Ans.pack()
 Area_Ans.pack()
 Windows.mainloop()
 
-"""
-Semi_Circle = input("Do you need semi circle calculations ")
-if Semi_Circle == "Yes" or "yes":
-    Diameter_Semi = float(input("Enter Diameter"))
-    Radius_Semi = Diameter_Semi / 2 or float(input("Enter Radius"))
+Windows_Semi = tk.Tk()
+Windows_Semi.title("Semi Circle Calculator")
+Semi_Circle = str(askstring("Semi Circles", "Do you need semi circle calculations ")).lower
+if Semi_Circle == "yes":
+    Diameter_Semi = float(askstring("Diamter", "Enter Diameter"))
+    Radius_Semi = Diameter_Semi/2 or float(askstring("Radius", "Enter Radius"))
     Diameter_Semi = Radius_Semi * 2
     Circumference_Semi = (2 * pi * Radius)/2
     Area_Semi = pi*(Radius**2)/2
 
-    print("Calculation complete")
-    print(f"Diameter is {Diameter_Semi}")
-    print(f"Radius is {Radius_Semi}")
-    print(f"Circumference is {Circumference_Semi}")
-    print(f"Area is {Area_Semi}")
+    Semi_Diameter_Ans = tk.Label(text=f"Diameter is {Diameter_Semi}")
+    Semi_Diameter_Ans.pack()
+    Semi_Radius_Ans = tk.Label(text=f"Radius is {Radius_Semi}")
+    Semi_Radius_Ans.pack()
+    Semi_Circumfernce_Ans = tk.Label(text=f"Circumfernce is {Circumference_Semi}")
+    Semi_Circumfernce_Ans.pack()
+    Semi_Area_Ans = tk.Label(text=f"Area is {Area_Semi}")
+    Semi_Area_Ans.pack()
+
 else:
     exit()
-"""
