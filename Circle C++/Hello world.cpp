44195e9c8075a668763b80f7aaff92e0169c35e2
Ant0069 <167881182+Ant0069@users.noreply.github.com>
2024-10-13 18:35:00 +0800

    c++

---
+++ Circle C++/Hello world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int pi = M_PI;
+
+    
+    return 0;
+}
---
+++ MMM.py
@@ -0,0 +1,7 @@
+import re
+import math
+
+
+String = input("Enter Numbers ")
+list = re.split('\s', String)
+print(list)
